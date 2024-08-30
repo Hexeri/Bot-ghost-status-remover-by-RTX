a6d5099337fb6a0df1aee15f5fec9802c0b3a35d
Hexeri <elfielding08@gmail.com>
2024-08-30 01:39:43 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Riding Rollercoasters","Booking tickets","Sharing information"];
 
 
 let currentIndex = 0;
